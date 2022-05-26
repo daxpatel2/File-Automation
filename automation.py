ff253b21a9fc618e80bc94c1f2cd78d0e9f61c09
UnmarkedBanshee <dxp38@pct.edu>
2022-05-25 22:16:34 -0400

    finalized script
    Finished initial python script with working functionality before any modifications

--- automation.py
+++ automation.py
@@ -7,21 +7,32 @@ USER = os.getenv('USER')
 #go to root directory where most files will be stored
 root_dir = '/Users/{}/Downloads/'.format(USER)
 #go to each root directory per file type:
-image_dir = '/Users/{}/Downloads/images/'.format(USER)
+img_dir = '/Users/{}/Downloads/images/'.format(USER)
 documents_dir = '/Users/{}/Downloads/documents/'.format(USER)
 others_dir = '/Users/{}/Downloads/others/'.format(USER)
 software_dir = '/Users/{}/Downloads/softwares/'.format(USER)
 
+#possible file types:
+document_types = ('.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pptx')
+img_types = ('.jpg', '.jpeg', '.png', '.svg', '.gif', '.tif', '.tiff', '.bmp')
+software_types = ('.exe', '.pkg', '.dmg', '.msi', '.app', '.deb', '.rpm', '.msp')
 
-#python has the move function which allows us to move files from one directory to another
+#grab all files in root directory
+def get_files(root_dir):
+    return [f for f in os.listdir(root_dir) if os.path.isfile(f) and not f.startswith('.') and not f.__eq__(__file__)]
 
+#to move files to thier proper directory using the 'move(item, destination)' function
+def move_files(files):
+    for file in files: #itteratig through the list with all our files
+        if files.endsWith(document_types):
+            move(file, '{}/{}'.format(documents_dir, file))
+        elif files.endsWith(img_types):
+            move(file, '{}/{}'.format(img_dir, file))
+        elif files.endsWith(software_types):
+            move(file, '{}/{}'.format(software_dir, file))
+        else:
+            move(file, '{}/{}'.format(others_dir, file))
 
-
-
-type_of_files_dict = {'word': 'docx', 'excel': 'xlsx', 'powerpoint': 'pptx', 'pdf': 'pdf', 'image': 'jpg', 'video': 'mp4', 'audio': 'mp3', 'text': 'txt'}
-#use dict comprehension if possible for more practice
-#write function to find the file type, then use that to move the file to the correct folder
-#we must take the downloads folder, access the files there, then move them to the correct folder using the folder's os path
-
-def find_type(file):
-    pass
+if __name__ == '__main__':
+    files = get_files(root_dir)
+    move_files(files)
