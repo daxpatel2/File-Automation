57d1eb4b8e3b251e6825feadbd6d2ab1cdf9a6cc
UnmarkedBanshee <dxp38@pct.edu>
2022-05-26 16:16:36 -0400

    fixed errors

--- automation.py
+++ automation.py
@@ -1,5 +1,5 @@
-import os
-from shutil import move
+import os #allows us to interact with the directory
+from shutil import move #offers high level file interaction
 
 #os.getenv('USER') gets the users environment variable (in our example it is the username)
 USER = os.getenv('USER')
@@ -17,21 +17,30 @@ document_types = ('.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pp
 img_types = ('.jpg', '.jpeg', '.png', '.svg', '.gif', '.tif', '.tiff', '.bmp')
 software_types = ('.exe', '.pkg', '.dmg', '.msi', '.app', '.deb', '.rpm', '.msp')
 
-#grab all files in root directory
+
+#grab all files in root directory excluding current file
 def get_files(root_dir):
     return [f for f in os.listdir(root_dir) if os.path.isfile(f) and not f.startswith('.') and not f.__eq__(__file__)]
 
-#to move files to thier proper directory using the 'move(item, destination)' function
+#to move files to thier proper directory using the 'move(item, destination)' function, THIS WILL OVERRIDE FILES IF THEY EXIST
 def move_files(files):
     for file in files: #itteratig through the list with all our files
-        if files.endsWith(document_types):
-            move(file, '{}/{}'.format(documents_dir, file))
-        elif files.endsWith(img_types):
-            move(file, '{}/{}'.format(img_dir, file))
-        elif files.endsWith(software_types):
-            move(file, '{}/{}'.format(software_dir, file))
+        if file.endswith(document_types):
+            if file not in documents_dir:
+                move(file, documents_dir)
+            #move(file, '{}/{}'.format(documents_dir, file))
+        elif file.endswith(img_types):
+            if file not in img_dir:
+                move(file, img_dir)
+            #move(file, '{}/{}'.format(img_dir, file))
+        elif file.endswith(software_types):
+            if file not in software_dir:
+                move(file, software_dir)
+            #move(file, '{}/{}'.format(software_dir, file))
         else:
-            move(file, '{}/{}'.format(others_dir, file))
+            if file not in others_dir:
+                move(file, others_dir)
+            #move(file, '{}/{}'.format(others_dir, file))
 
 if __name__ == '__main__':
     files = get_files(root_dir)
