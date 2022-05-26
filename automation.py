e4b5166bb37606007e21b70ac6eec3a282f68571
UnmarkedBanshee <dxp38@pct.edu>
2022-05-25 21:07:13 -0400

    started the __init__

---
+++ automation.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+type_of_files_dict = {'word': 'docx', 'excel': 'xlsx', 'powerpoint': 'pptx', 'pdf': 'pdf', 'image': 'jpg', 'video': 'mp4', 'audio': 'mp3', 'text': 'txt'}
+
+#write function to find the file type, then use that to move the file to the correct folder
+#we must take the downloads folder, access the files there, then move them to the correct folder using the folder's os path
+
+def find_type(file):
+    pass
