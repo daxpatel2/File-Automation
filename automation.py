f14c1f87b47bd5b678aeb2a3f1112828ff32b7ff
UnmarkedBanshee <dxp38@pct.edu>
2022-05-30 14:54:33 -0400

    "update" new directories suppprt
    Added support for desktop directory, with fucntionlity to add any directry the user wants

--- automation.py
+++ automation.py
@@ -1,7 +1,9 @@
 import os #allows us to interact with the directory
+import sys #allow the usage of argv cmmd
 from shutil import move #offers high level file interaction
 import datetime #to write to current time to log file
 
+#we need better logging functionality so the user can check log file for all movements of files
 with open('/Users/daxpatel/Downloads/Log/log.txt', 'a') as f:
     log = f
     log.write('File was read on {}\n'.format(datetime.datetime.now()))
@@ -12,61 +14,76 @@ USER = os.getenv('USER')
 #directories that we want to clean
 root_dir = '/Users/{}/Downloads/'.format(USER)
 desktop_dir = '/Users/{}/Desktop/'.format(USER)
-#go to each root directory per file type:
-img_dir = '/Users/{}/Downloads/images/'.format(USER)
-documents_dir = '/Users/{}/Downloads/documents/'.format(USER)
-others_dir = '/Users/{}/Downloads/others/'.format(USER)
-software_dir = '/Users/{}/Downloads/softwares/'.format(USER)
 
-#possible file types:
+#go to each downloads directory per file type:
+img_dir_downloads = '/Users/{}/Downloads/images/'.format(USER)
+documents_dir_downloads = '/Users/{}/Downloads/documents/'.format(USER)
+others_dir_downloads = '/Users/{}/Downloads/others/'.format(USER)
+software_dir_downloads = '/Users/{}/Downloads/softwares/'.format(USER)
+#go to each Desktop directory per file type:
+desktop_img_dir = '/Users/{}/Desktop/images/'.format(USER)
+desktop_documents_dir = '/Users/{}/Desktop/documents/'.format(USER)
+desktop_others_dir = '/Users/{}/Desktop/others/'.format(USER)
+desktop_software_dir = '/Users/{}/Desktop/softwares/'.format(USER)
+
+#all possible file types:
 document_types = ('.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pptx')
 img_types = ('.jpg', '.jpeg', '.png', '.svg', '.gif', '.tif', '.tiff', '.bmp')
 software_types = ('.exe', '.pkg', '.dmg', '.msi', '.app', '.deb', '.rpm', '.msp')
 
-#grab all files in root directory excluding current file
-def get_files(root_dir):
-    return [f for f in os.listdir(root_dir) if os.path.isfile(f) and not f.startswith('.') and not f.__eq__(__file__)]
+#grab all files in specifeid directory using argv
+argv = sys.argv[1]
+if argv == 'downloads':
+    go_dir = root_dir
+elif argv == 'desktop':
+    go_dir = desktop_dir
 
+#specify the directory to search for files and grab a list of all the files in that directory
+def get_files(go_dir):
+    return [f for f in os.listdir(go_dir) if os.path.isfile(f) and not f.startswith('.') and not f.__eq__(__file__)]
+
+files = get_files(go_dir)
 #to move files to thier proper directory using the 'move(item, destination)' function, THIS WILL OVERRIDE FILES IF THEY EXIST
 def move_files(files):
     for file in files: #itteratng through the list with all our files
         if file.endswith(document_types):
-            if file not in documents_dir:
+            if file not in go_dir+'documents/':
                 try:
-                    move(file, documents_dir)
+                    move(file, go_dir+'documents/')
                     #log.write('Successfully moved {} to Documents folder on {}\n'.format(file, datetime.datetime.now()))
                     #write to the log file 
                 except:
                     log.write('Failed to move {} to Documents folder\n'.format(file))
                     #write to the log file in caps for errors
 
-            #move(file, '{}/{}'.format(documents_dir, file))
+            #move(file, '{}/{}'.format(documents_dir_downloads, file))
         elif file.endswith(img_types):
-            if file not in img_dir:
+            if file not in go_dir+'images/':
                 try:
-                    move(file, documents_dir)
+                    move(file, go_dir+'images/')
                     #log.write('Successfully moved {} to Images folder on {}\n'.format(file, datetime.datetime.now()))
                 except:
                     log.write('Failed to move {} to Images folder\n'.format(file))
-                    #move(file, '{}/{}'.format(img_dir, file))
+                    #move(file, '{}/{}'.format(img_dir_downloads, file))
 
         elif file.endswith(software_types):
-            if file not in software_dir:
+            if file not in go_dir+'softwares/':
                 try:
-                    move(file, documents_dir)
+                    move(file, go_dir+'softwares/')
                     #log.write('Successfully moved {} to Softwares folder {}\n'.format(file,datetime.datetime.now()))
                 except:
                     log.write('Failed to move {} to Softwares folder\n'.format(file))
-                    #move(file, '{}/{}'.format(software_dir, file))
+                    #move(file, '{}/{}'.format(software_dir_downloads, file))
         else:
-            if file not in others_dir:
+            if file not in go_dir+'others/':
                 try:
-                    move(file, documents_dir)
+                    move(file, go_dir+'others/')
                     #log.write('Successfully moved {} to Others folder on {}\n'.format(file, datetime.datetime.now()))
                 except:
                     log.write('Failed to move {} to Others folder\n'.format(file))
-                    #move(file, '{}/{}'.format(others_dir, file))
+                    #move(file, '{}/{}'.format(others_dir_downloads, file))
 
+#will have to fix this to move files to their proper directory
 def main():
     if __name__ == '__main__':
         files = get_files(root_dir)
