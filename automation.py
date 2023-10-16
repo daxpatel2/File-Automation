7c8561cc1e4c5e9556d7843938e0e37ed52afb56
UnmarkedBanshee <dxp38@pct.edu>
2023-10-16 16:48:58 -0400

    upadtes to code structure

--- automation.py
+++ automation.py
@@ -3,10 +3,18 @@ import sys #allow the usage of argv cmmd
 from shutil import move #offers high level file interaction
 import datetime #to write to current time to log file
 
+
+def mkdirs(typeOf):
+    print(currDir)
+    if not os.path.isdir(currDir+"/{}".format(typeOf)):
+        os.mkdir("{}".format(typeOf))
+
+
 #all possible file types:
-document_types = ('.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pptx')
-img_types = ('.jpg', '.jpeg', '.png', '.svg', '.gif', '.tif', '.tiff', '.bmp')
-software_types = ('.exe', '.pkg', '.dmg', '.msi', '.app', '.deb', '.rpm', '.msp')
+document_types = ('.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pptx','.zip')
+img_types = ('.JPG', '.jpeg', '.png', '.svg', '.gif', '.tif', '.tiff', '.bmp','.psd','.HEIC')
+software_types = ('.exe', '.pkg', '.dmg', '.msi', '.app', '.deb', '.rpm', '.msp','.iso')
+code_types = ('.py','.java','.html','.js','.jsx')
 
 #current working dir
 currDir = str(os.getcwd())
@@ -15,54 +23,57 @@ files = [f for f in os.listdir(".") if os.path.isfile(f) and not f.startswith('.
 
 logFile = open(currDir+"/log.txt","a")
 
-if not os.path.isdir(currDir+"/failed"):
-    os.mkdir("failed")
+mkdirs("img")
+for idx,file in enumerate(files):
+    if file.endswith(img_types) and file not in currDir+"img/":
+        try:
+            move(file, currDir+"/img/")
+            logFile.write('Successfully moved {} to img folder on {}\n'.format(file, datetime.datetime.now()))
+            del files[idx]
+        except Exception as e:
+            if isinstance(e, FileExistsError):
+                move(file, currDir+"/failed/")
+                print("failed to move {}, File already Existed, moving to failed directory".format(file))
+            logFile.write("Failed to move file {} -> {} on {}\n".format(file,e,datetime.datetime.now()))
 
-if os.path.isdir(currDir+"/img"):
-    for idx,file in enumerate(files):
-        if file.endswith(img_types) and file not in currDir+"img/":
-            try:
-                move(file, currDir+"/img/")
-                logFile.write('Successfully moved {} to img folder on {}\n'.format(file, datetime.datetime.now()))
-                del files[idx]
-            except Exception as e:
-                if isinstance(e, FileExistsError):
-                    print("similar error")
-                    move(file, currDir+"/failed/")
-                    print("failed to move {}, File already Existed, moving to failed directory".format(file))
-                logFile.write("Failed to move file {}, because {}\n".format(file,e))
-else:
-    os.mkdir("img")
+mkdirs("docs")
+for idx,file in enumerate(files):
+    if file.endswith(document_types) and file not in currDir+"docs/" and file != "log.txt":
+        try:
+            move(file, currDir+"/docs/")
+            logFile.write('Successfully moved {} to docs folder on {}\n'.format(file, datetime.datetime.now()))
+            del files[idx]
+        except Exception as e:
+            if isinstance(e, FileExistsError):
+                move(file,currDir+"/failed/")
+                print("failed to move {}, File already Existed, moving to failed directory".format(file))
+            logFile.write("Failed to move file {}, because {} on {}\n".format(file,e,datetime.datetime.now()))
 
-if os.path.isdir(currDir+"/docs"):
-    for idx,file in enumerate(files):
-        if file.endswith(document_types) and file not in currDir+"docs/" and file != "log.txt":
-            try:
-                move(file, currDir+"/docs/")
-                logFile.write('Successfully moved {} to docs folder on {}\n'.format(file, datetime.datetime.now()))
-                del files[idx]
-            except Exception as e:
-                if isinstance(e, FileExistsError):
-                    move(file,currDir+"/failed/")
-                    print("failed to move {}, File already Existed, moving to failed directory".format(file))
-                logFile.write("Failed to move file {}, because {}\n".format(file,e))
-else:
-    os.mkdir("docs")
+mkdirs("software")
+for idx,file in enumerate(files):
+    if file.endswith(software_types) and file not in currDir+"software/":
+        try:
+            move(file, currDir+"/software/")
+            logFile.write('Successfully moved {} to software folder on {}\n'.format(file, datetime.datetime.now()))
+            del files[idx]
+        except Exception as e:
+            if isinstance(e, FileExistsError):
+                move(file,currDir+"/failed/")
+                print("failed to move {}, File already Existed, moving to failed directory".format(file))
+            logFile.write("Failed to move file {}, because {} on {}\n".format(file,e,datetime.datetime.now()))
 
-if os.path.isdir(currDir+"/software"):
-    for idx,file in enumerate(files):
-        if file.endswith(software_types) and file not in currDir+"software/":
-            try:
-                move(file, currDir+"/software/")
-                logFile.write('Successfully moved {} to software folder on {}\n'.format(file, datetime.datetime.now()))
-                del files[idx]
-            except Exception as e:
-                if isinstance(e, FileExistsError):
-                    move(file,currDir+"/failed/")
-                    print("failed to move {}, File already Existed, moving to failed directory".format(file))
-                logFile.write("Failed to move file {}, because {}\n".format(file,e))
-else:
-    os.mkdir("software")
+mkdirs("code")
+for idx,file in enumerate(files):
+    if file.endswith(code_types) and file not in currDir+"code/":
+        try:
+            move(file, currDir+"/code/")
+            logFile.write('Successfully moved {} to code folder on {}\n'.format(file, datetime.datetime.now()))
+            del files[idx]
+        except Exception as e:
+            if isinstance(e, FileExistsError):
+                move(file,currDir+"/failed/")
+                print("failed to move {}, File already Existed, moving to failed directory".format(file))
+            logFile.write("Failed to move file {}, because {} on {}\n".format(file,e,datetime.datetime.now()))
 
 logFile.close()
 
